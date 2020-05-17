136bc20ebb07a5b96bd7bba6ec4309198e80993e
604849hiro.s@gmail.com <604849hiro.s@gmail.com>
2020-05-17 06:54:04 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum:50 }
@@ -32,7 +32,7 @@ class User < ApplicationRecord
     # 渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticated?(attribute, token)
         digest = self.send("#{attribute}_digest")
-        return false if remember_digest.nil?
+        return false if digest.nil?
         BCrypt::Password.new(digest).is_password?(token)
     end
     
@@ -51,6 +51,21 @@ class User < ApplicationRecord
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
+    
+    # パスワード再設定の属性を設定する
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token),reset_sent_at: Time.zone.now)
+    end
+    
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+    
+    # パスワード再設定の期限が切れている場合はtrueを返す
+     def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+     end
     
     private
     
